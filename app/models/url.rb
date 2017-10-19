cd512360720410e9671230e9dec66b5340620f29
Saeed Gofur <saeedgofur@oto918-3.local>
2017-10-19 12:02:27 +0800

    Copied the shorten method from previous bitly clone along with validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,23 @@
 class Url < ApplicationRecord
+
+	validates :long_url, presence: true, uniqueness: {case_sensitive: false, message: "was shortened before"}
+
+  	validates :long_url, format: {with: (URI::regexp(['http', 'https'])), message: "format is not good"}
+
+
+	def short
+
+		array = ("0".."9").to_a + ("a".."z").to_a + ("A".."Z").to_a
+
+		@short_url = []
+
+		7.times do
+			@short_url << array.sample
+		end
+
+		shorten = @short_url.join
+		
+    	return shorten
+	end
+
 end
